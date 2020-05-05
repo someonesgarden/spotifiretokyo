640f0fd22d1aee557f58af191f8d91bfd4fdc19d
Daisuke Nishimura <d@someonesgarden.org>
2020-05-06 01:38:47 +0900

    scss enabled

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -188,6 +188,25 @@ module.exports = {
               },
             ],
           },
+
+          {
+            test: /\.scss/,
+            use: [
+              'style-loader',
+              {
+                  loader: require.resolve('css-loader'),
+                options: {
+                  url: false,
+
+                  importLoaders: 2
+                },
+              },
+              {
+                loader: require.resolve('sass-loader'),
+
+              }
+            ],
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import './App.css';
+import './scss/App.scss';
 import Header from './components/spotify/Header';
 import Footer from './components/spotify/Footer';
 import UserPlaylists from './components/spotify/UserPlaylists';
